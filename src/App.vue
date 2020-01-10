1cac299bb6f1ed4800ed18b0a83aa1750b1c134a
Patrick-web <pntxall100@gmail.com>
2020-01-10 12:28:49 -0800

    modified

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,607 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+     <!-- SNACKBAR -->
+      <div class="text-center ma-2">
+          <v-btn dark @click="snackbar = true">Open Snackbar</v-btn>
+          <v-snackbar
+            v-model="snackbar"
+          >
+            {{ text }}
+            <v-btn
+              color="pink"
+              text
+              @click="snackbar = false"
+            >
+              Close
+            </v-btn>
+          </v-snackbar>
+        </div>
+     <!-- SNACKBAR -->
+
+<!-- MODALS -->
+      <div class="text-center">
+          <v-dialog
+            v-model="dialog"
+            width="500"
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+                color="red lighten-2"
+                dark
+                v-on="on"
+              >
+                Click Me
+              </v-btn>
+            </template>
+
+            <v-card>
+              <v-card-title
+                class="headline grey lighten-2"
+                primary-title
+              >
+                Privacy Policy
+              </v-card-title>
+
+              <v-card-text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </v-card-text>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="primary"
+                  text
+                  @click="dialog = false"
+                >
+                  I accept
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+<!-- MODALS -->
+
+      <!-- CHIPS -->
+        <div
+          id="chip-usage-example"
+          class="text-center"
+        >
+          <v-chip>Default</v-chip>
+          <v-chip close>Close</v-chip>
+          <v-chip outlined>Outlined</v-chip>
+          <v-chip v-model="value" filter>Filter</v-chip>
+          <v-chip pill>
+            <v-avatar
+              left
+              color="red"
+            >
+              P
+            </v-avatar>
+            Pill
+          </v-chip>
+          <v-chip>
+            <v-icon left color="red">mdi-account</v-icon>
+            Icon
+          </v-chip>
+        </div>
+      <!-- CHIPS -->
+
+
+
+     <v-carousel>
+        <v-carousel-item
+ v-for="(item,i) in 5"
+            :key="i"
+        >
+          <v-sheet
+            :color="color"
+            height="100%"
+            tile
+          >
+            <v-row
+              class="fill-height"
+              align="center"
+              justify="center"
+            >
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci incidunt consequuntur veniam nostrum nisi necessitatibus at, aliquid sint natus delectus!</p>
+              <div class="display-3">Slide {{ i + 1 }}</div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
+        <v-expansion-panels>
+          <v-expansion-panel
+            v-for="(item,i) in 5"
+            :key="i"
+          >
+            <v-expansion-panel-header>Item</v-expansion-panel-header>
+            <v-expansion-panel-content>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+          <v-tabs>
+            <v-tab>Item One</v-tab>
+            <v-tab>Item Two</v-tab>
+            <v-tab>Item Three</v-tab>
+          </v-tabs>
+           <v-timeline :dense="$vuetify.breakpoint.smAndDown">
+    <v-timeline-item
+      color="purple lighten-2"
+      fill-dot
+      right
+    >
+      <v-card>
+        <v-card-title class="purple lighten-2">
+          <v-icon
+            dark
+            size="42"
+            class="mr-4"
+          >
+            mdi-magnify
+          </v-icon>
+          <h2 class="display-1 white--text font-weight-light">Title 1</h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="10">
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
+            </v-col>
+            <v-col
+              class="hidden-sm-and-down text-right"
+              md="2"
+            >
+              <v-icon size="64">mdi-calendar-text</v-icon>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="amber lighten-1"
+      fill-dot
+      left
+      small
+    >
+      <v-card>
+        <v-card-title class="amber lighten-1 justify-end">
+          <h2 class="display-1 mr-4 white--text font-weight-light">Title 2</h2>
+          <v-icon
+            dark
+            size="42"
+          >mdi-home-outline</v-icon>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="8">
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
+            </v-col>
+            <v-col cols="12" md="4">
+              Lorem ipsum dolor sit amet, no nam oblique veritus.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="cyan lighten-1"
+      fill-dot
+      right
+    >
+      <v-card>
+        <v-card-title class="cyan lighten-1">
+          <v-icon
+            class="mr-4"
+            dark
+            size="42"
+          >
+            mdi-email-outline
+          </v-icon>
+          <h2 class="display-1 white--text font-weight-light">Title 3</h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col
+              v-for="n in 3"
+              :key="n"
+              cols="12"
+              md="4"
+            >
+              Lorem ipsum dolor sit amet, no nam oblique veritus no nam oblique.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="red lighten-1"
+      fill-dot
+      left
+      small
+    >
+      <v-card>
+        <v-card-title class="red lighten-1 justify-end">
+          <h2 class="display-1 mr-4 white--text font-weight-light">Title 4</h2>
+          <v-icon
+            dark
+            size="42"
+          >
+            mdi-account-multiple-outline
+          </v-icon>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col class="hidden-sm-and-down" md="2">
+              <v-icon size="64">mdi-server-network</v-icon>
+            </v-col>
+            <v-col cols="12" md="10">
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="green lighten-1"
+      fill-dot
+      right
+    >
+      <v-card>
+        <v-card-title class="green lighten-1">
+          <v-icon
+            class="mr-4"
+            dark
+            size="42"
+          >
+            mdi-phone-in-talk
+          </v-icon>
+          <h2 class="display-1 white--text font-weight-light">Title 5</h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col>
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
+
+<!-- BADGES -->
+ <v-toolbar>
+    <v-tabs
+      dark
+      background-color="primary"
+      grow
+    >
+      <v-tab>
+        <v-badge
+          color="pink"
+          dot
+        >
+          Item One
+        </v-badge>
+      </v-tab>
+
+      <v-tab>
+        <v-badge
+          color="green"
+          content="6"
+        >
+          Item Two
+        </v-badge>
+      </v-tab>
+
+      <v-tab>
+        <v-badge
+          color="deep-purple accent-4"
+          icon="mdi-vuetify"
+        >
+          Item Three
+        </v-badge>
+      </v-tab>
+    </v-tabs>
+  </v-toolbar>
+
+  <!-- FABS -->
+   <v-card id="create">
+
+    <v-speed-dial
+
+    >
+      <template v-slot:activator>
+        <v-btn
+          v-model="fab"
+          color="blue darken-2"
+          dark
+          fab
+        >
+          <v-icon v-if="fab">mdi-close</v-icon>
+          <v-icon v-else>mdi-account-circle</v-icon>
+        </v-btn>
+      </template>
+      <v-btn
+        fab
+        dark
+        small
+        color="green"
+      >
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="indigo"
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="red"
+      >
+        <v-icon>mdi-delete</v-icon>
+      </v-btn>
+    </v-speed-dial>
+  </v-card>
+<!-- BOTTOM NAV -->
+<v-bottom-navigation
+    :value="activeBtn"
+    grow
+    color="teal"
+  >
+    <v-btn>
+      <span>Recents</span>
+      <v-icon>mdi-history</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Favorites</span>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Nearby</span>
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+
+<!-- TIMEPICKER -->
+  <v-row justify="center">
+    <v-time-picker v-model="picker"></v-time-picker>
+  </v-row>
+  <!-- ALERTS -->
+<v-container>
+  <v-alert
+      dense
+      type="info"
+    >
+      I'm a dense alert with a <strong>type</strong> of info
+    </v-alert>
+    <v-alert
+      dense
+      text
+      type="success"
+    >
+      I'm a dense alert with the <strong>text</strong> prop and a <strong>type</strong> of success
+    </v-alert>
+    <v-alert
+      dense
+      border="left"
+      type="warning"
+    >
+      I'm a dense alert with the <strong>border</strong> prop and a <strong>type</strong> of warning
+    </v-alert>
+    <v-alert
+      dense
+      outlined
+      type="error"
+    >
+      I'm a dense alert with the <strong>outlined</strong> prop and a <strong>type</strong> of error
+    </v-alert>
+    </v-container>
+
+<!-- LINER PROGRESS -->
+<v-progress-linear value="100"></v-progress-linear>
+
+  <!-- TODO -->
+<v-container style="max-width: 500px">
+  <v-text-field
+      v-model="task"
+      label="What are you working on?"
+      solo
+      @keydown.enter="create"
+    >
+      <v-fade-transition v-slot:append>
+        <v-icon
+          v-if="task"
+          @click="create"
+        >
+          add_circle
+        </v-icon>
+      </v-fade-transition>
+    </v-text-field>
+
+    <h2 class="display-1 success--text pl-4">
+      Tasks:&nbsp;
+      <v-fade-transition leave-absolute>
+        <span :key="`tasks-${tasks.length}`">
+          {{ tasks.length }}
+        </span>
+      </v-fade-transition>
+    </h2>
+
+    <v-divider class="mt-4"></v-divider>
+
+    <v-row
+      class="my-1"
+      align="center"
+    >
+      <strong class="mx-4 info--text text--darken-3">
+        Remaining: {{ remainingTasks }}
+      </strong>
+
+      <v-divider vertical></v-divider>
+
+      <strong class="mx-4 black--text">
+        Completed: {{ completedTasks }}
+      </strong>
+
+      <v-spacer></v-spacer>
+
+      <v-progress-circular
+        :value="progress"
+        class="mr-2"
+      ></v-progress-circular>
+    </v-row>
+
+    <v-divider class="mb-4"></v-divider>
+
+    <v-card v-if="tasks.length > 0">
+      <v-slide-y-transition
+        class="py-0"
+        group
+        tag="v-list"
+      >
+        <template v-for="(task, i) in tasks">
+          <v-divider
+            v-if="i !== 0"
+            :key="`${i}-divider`"
+          ></v-divider>
+
+          <v-list-item :key="`${i}-${task.text}`">
+            <v-list-item-action>
+              <v-checkbox
+                v-model="task.done"
+                color="info darken-3"
+              >
+                <template v-slot:label>
+                  <div
+                    :class="task.done && 'grey--text' || 'text--primary'"
+                    class="ml-4"
+                    v-text="task.text"
+                  ></div>
+                </template>
+              </v-checkbox>
+            </v-list-item-action>
+
+            <v-spacer></v-spacer>
+
+            <v-scroll-x-transition>
+              <v-icon
+                v-if="task.done"
+                color="success"
+              >
+                check
+              </v-icon>
+            </v-scroll-x-transition>
+          </v-list-item>
+        </template>
+      </v-slide-y-transition>
+    </v-card>
+      </v-container>
+      </v-container>
+    </v-content>
+
+    <!-- <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer> -->
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+      picker: null,
+      snackbar: false,
+      text: 'Hello, I\'m a snackbar',
+      tasks: [
+        {
+          done: false,
+          text: 'Foobar',
+        },
+        {
+          done: false,
+          text: 'Fizzbuzz',
+        },
+      ],
+      task: null,
+    }),
+    created () {
+      this.$vuetify.theme.dark = true
+    },
+    computed: {
+      completedTasks () {
+        return this.tasks.filter(task => task.done).length
+      },
+      progress () {
+        return this.completedTasks / this.tasks.length * 100
+      },
+      remainingTasks () {
+        return this.tasks.length - this.completedTasks
+      },
+    },
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+    methods: {
+      create () {
+        this.tasks.push({
+          done: false,
+          text: this.task,
+        })
+
+        this.task = null
+      },
+    },
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
